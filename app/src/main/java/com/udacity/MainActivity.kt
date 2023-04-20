d377de79acbb25c64995847ce1671d0041aee191
PaoloCaldera <paolo.caldera@outlook.com>
2023-04-20 23:21:03 +0200

    Add the radio group to the main activity

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -9,6 +9,9 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,15 +26,40 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
 
+    private lateinit var radioGroup: RadioGroup
+    private lateinit var glideRadioButton: RadioButton
+    private lateinit var loadAppRadioButton: RadioButton
+    private lateinit var retrofitRadioButton: RadioButton
+
+    private var url = URL
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        radioGroup = findViewById(R.id.download_options_radioGroup)
+        glideRadioButton = findViewById(R.id.glide_radioButton)
+        loadAppRadioButton = findViewById(R.id.loadapp_radioButton)
+        retrofitRadioButton = findViewById(R.id.retrofit_radioButton)
+
+        glideRadioButton.setOnClickListener {
+            url = getString(R.string.url_glide)
+        }
+        loadAppRadioButton.setOnClickListener {
+            url = getString(R.string.url_loadapp)
+        }
+        retrofitRadioButton.setOnClickListener {
+            url = getString(R.string.url_retrofit)
+        }
+
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         custom_button.setOnClickListener {
-            download()
+            if (radioGroup.checkedRadioButtonId == -1)
+                Toast.makeText(this, getString(R.string.toast_message), Toast.LENGTH_SHORT).show()
+            else
+                download(url)
         }
     }
 
@@ -41,9 +69,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun download() {
+    private fun download(url: String) {
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(url))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
